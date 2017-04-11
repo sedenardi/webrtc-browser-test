8f89eb95a30d81e46edda5ae71cf403e9dda5ea5
Sanders DeNardi <sedenardi@gmail.com>
2017-04-11 15:33:13 -0400

    add audio connection and volume meter

--- src/webrtc-browser-test.js
+++ src/webrtc-browser-test.js
@@ -8,11 +8,29 @@
 
   const WebRtcBrowserTest = function(opts) {
 
-    if (typeof opts.videoElementContainer === 'string') {
-      opts.videoElementContainer = document.querySelector(opts.videoElementContainer);
+    if (typeof opts.mediaElementContainer === 'string') {
+      opts.mediaElementContainer = document.querySelector(opts.mediaElementContainer);
     }
-    if (typeof opts.volumeMeterElement === 'string') {
-      opts.volumeMeterElement = document.querySelector(opts.volumeMeterElement);
+    if (!opts.mediaElementContainer) {
+      throw customError('ParameterError', 'Missing required element parameter');
+    }
+
+    if (!opts.onVolumeChange) {
+      throw customError('ParameterError', 'Missing required volume callback parameter');
+    }
+
+    let videoElement = document.querySelector('#videoContainer > video');
+    if (!videoElement) {
+      videoElement = document.createElement('video');
+      videoElement.setAttribute('autoplay', true);
+      opts.mediaElementContainer.appendChild(videoElement);
+    }
+
+    let audioElement = document.querySelector('#videoContainer > audio');
+    if (!audioElement) {
+      audioElement = document.createElement('audio');
+      audioElement.setAttribute('autoplay', true);
+      opts.mediaElementContainer.appendChild(audioElement);
     }
 
     const getUserMedia = function(constraints) {
@@ -44,12 +62,6 @@
     };
 
     const startVideo = function() {
-      let videoElement = document.querySelector('#videoContainer > video');
-      if (!videoElement) {
-        videoElement = document.createElement('video');
-        videoElement.setAttribute('autoplay', true);
-        opts.videoElementContainer.appendChild(videoElement);
-      }
       return checkBrowser().then(() => {
         return getUserMedia({ video: true }).then((stream) => {
           videoElement.srcObject = stream;
@@ -58,25 +70,53 @@
       });
     };
 
-    // const startAudio = function() {
-    //   return checkBrowser().then(() => {
-    //
-    //   });
-    // };
-    //
-    // const startTest = function() {
-    //   return checkBrowser().then(() => {
-    //     return startVideo();
-    //   }).then(() => {
-    //     return startAudio();
-    //   });
-    // };
+    const startAudio = function() {
+      return checkBrowser().then(() => {
+        return getUserMedia({ audio: true }).then((stream) => {
+          audioElement.srcObject = stream;
+          return startVolume(stream);
+        });
+      });
+    };
+
+    const createVolumeListener = function(audioContext, cb) {
+      const processor = audioContext.createScriptProcessor(2048, 1, 1);
+      processor.connect(audioContext.destination);
+      processor.onaudioprocess = function(event) {
+        const data = event.inputBuffer.getChannelData(0);
+        let i = 0;
+        let total = 0;
+        while (i < data.length) {
+          total += Math.abs(data[i++]);
+        }
+        const rms = Math.sqrt(total / data.length);
+        if (cb) { cb(rms); }
+      };
+      return processor;
+    };
+
+    const startVolume = function(stream) {
+      let audioContext;
+      try {
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        audioContext = new AudioContext();
+      } catch (e) {
+        return Promise.reject(customError('BrowserNotSupported', 'Your browser doesn\'t support web audio.'));
+      }
+      const listener = createVolumeListener(audioContext, opts.onVolumeChange);
+      try {
+        const mic = audioContext.createMediaStreamSource(stream);
+        mic.connect(listener);
+        return Promise.resolve();
+      } catch (e) {
+        return Promise.reject(e);
+      }
+    };
 
     return {
       checkBrowser,
       startVideo,
-      // startAudio,
-      // startTest
+      startAudio
     };
   };
 
